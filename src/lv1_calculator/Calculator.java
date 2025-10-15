242b34935ae3c56b047da9bef8aa3f32ce154918
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-15 16:13:47 +0900

    계산 지속 여부 확인 후 종료 기능 추가

--- src/lv1_calculator/Calculator.java
+++ src/lv1_calculator/Calculator.java
@@ -6,51 +6,57 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int num1;
-        int num2;
-        char mark;
+        while(true) {
 
-        while (true) {
-            System.out.print("첫 번째 숫자를 입력하세요: ");
-            num1 = scanner.nextInt();
+            int num1;
+            int num2;
+            char mark;
+            String exit;
 
-            if (num1 >= 0) {
-                break;
-            } else {
-                System.out.println("잘못된 숫자입니다. 양의 정수를 입력해주세요.");
+            // 슷자1 입력받기
+            while (true) {
+                System.out.print("첫 번째 숫자를 입력하세요: ");
+                num1 = scanner.nextInt();
+
+                if (num1 >= 0) {
+                    break;
+                } else {
+                    System.out.println("잘못된 숫자입니다. 양의 정수를 입력해주세요.");
+                }
             }
-        }
 
-        while (true) {
-            System.out.print("두 번째 숫자를 입력하세요: ");
-            num2 = scanner.nextInt();
+            // 숫자2 입력받기
+            while (true) {
+                System.out.print("두 번째 숫자를 입력하세요: ");
+                num2 = scanner.nextInt();
 
-            if (num2 >= 0) {
-                break;
-            } else {
-                System.out.println("잘못된 숫자입니다. 양의 정수를 입력해주세요.");
+                if (num2 >= 0) {
+                    break;
+                } else {
+                    System.out.println("잘못된 숫자입니다. 양의 정수를 입력해주세요.");
+                }
             }
-        }
 
-        while (true) {
-            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
-            char getmark = scanner.next().charAt(0);
+            // 사칙연산 기호 입력받기
+            while (true) {
+                System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+                char getmark = scanner.next().charAt(0);
 
-            if (getmark == '+' || getmark == '-' || getmark == '*' || getmark == '/') {
-                mark = getmark;
-                break;
-            } else if (getmark == 'x' || getmark == 'X') {
-                mark = '*';
-                break;
-            } else if (getmark == '%') {
-                mark = '/';
-                break;
-            } else {
-                System.out.println("잘못된 기호입니다. 사칙연산 기호(+, -, *, /) 중 하나를 입력해주세요.");
+                if (getmark == '+' || getmark == '-' || getmark == '*' || getmark == '/') {
+                    mark = getmark;
+                    break;
+                } else if (getmark == 'x' || getmark == 'X') {
+                    mark = '*';
+                    break;
+                } else if (getmark == '%') {
+                    mark = '/';
+                    break;
+                } else {
+                    System.out.println("잘못된 기호입니다. 사칙연산 기호(+, -, *, /) 중 하나를 입력해주세요.");
+                }
             }
-        }
 
-        // if문으로 작성
+            // if문으로 작성
 //        int result = 0;
 //        if (mark == '+') {
 //            result = num1 + num2;
@@ -75,37 +81,48 @@ public class Calculator {
 //            }
 //        }
 
-
-        // switch문으로 작성
-        int result = 0;
-        switch (mark) {
-            case '+' -> {
-                result = num1 + num2;
-                System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
-            }
-            case '-' -> {
-                result = num1 - num2;
-                System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
-            }
-            case '*' -> {
-                result = num1 * num2;
-                System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
-            }
-            case '/' -> {
-                if (num2 == 0) {
-                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다");
-                } else if (num2 > num1) {
-                    double d_num1 = num1;
-                    double d_num2 = num2;
-                    double result2 = d_num1 / d_num2;
-                    System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result2);
-                } else {
-                    result = num1 / num2;
+            // switch문으로 작성
+            int result = 0;
+            switch (mark) {
+                case '+' -> {
+                    result = num1 + num2;
                     System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
                 }
+                case '-' -> {
+                    result = num1 - num2;
+                    System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
+                }
+                case '*' -> {
+                    result = num1 * num2;
+                    System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
+                }
+                case '/' -> {
+                    if (num2 == 0) {
+                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다");
+                    } else if (num2 > num1) {
+                        double d_num1 = num1;
+                        double d_num2 = num2;
+                        double result2 = d_num1 / d_num2;
+                        System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result2);
+                    } else {
+                        result = num1 / num2;
+                        System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
+                    }
+                }
+                default -> result = 0;
+            };
+
+            // 지속 여부 확인
+            scanner.nextLine();
+            System.out.println("더 계산하시겠습니까?(exit 입력 시 종료): ");
+            exit = scanner.nextLine();
+
+            if (!exit.equals("exit")) {
+                System.out.println("새로운 계산을 시작합니다.");
+            } else {
+                break;
             }
-            default -> result = 0;
-        };
+        }
 
     }
 }
