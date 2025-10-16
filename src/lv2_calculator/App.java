8f46a2b1f5bdf7bbc4d70b4890a24eb20f7c84b7
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-16 15:33:47 +0900

    App 클래스의 메인 메서드에 Calculator 클래스 활용

---
+++ src/lv2_calculator/App.java
@@ -0,0 +1,37 @@
+package lv2_calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char mark = scanner.next().charAt(0);
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+
+            int result = calculator.Calculate(num1, mark, num2);
+            System.out.println("계산 결과: " + num1 + " " + mark + " " + num2 + " = " + result);
+
+            scanner.nextLine();
+            System.out.println("더 계산하시겠습니까?(exit 입력 시 종료): ");
+            String exit = scanner.nextLine();
+
+            calculator.results.add(result);
+            System.out.println("현재까지 진행된 계산 결과: " + calculator.results);
+
+            if (!exit.equals("exit")) {
+                System.out.println("새로운 계산을 시작합니다.");
+            } else {
+                break;
+            }
+        }
+    }
+}
