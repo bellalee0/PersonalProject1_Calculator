83aeea22997d22aa26e3771121987235186ad0d6
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-16 15:00:47 +0900

    Calculator 클래스 생성 - 사칙연산 수행 및 결과값 반환 메서드 - 연산 결과 저장하는 컬렉션 타입 필드

--- src/lv2_calculator/Calculator.java
+++ src/lv2_calculator/Calculator.java
@@ -1,7 +1,19 @@
 package lv2_calculator;
 
-public class Calculator {
-    public static void main(String[] args) {
+import java.util.ArrayList;
 
+public class Calculator {
+    ArrayList<Integer> results = new ArrayList<Integer>();
+
+    public int Calculate(int num1, char mark, int num2){
+        int result = 0;
+        result = switch(mark){
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*' -> num1 * num2;
+            case '/' -> num1 / num2;
+            default -> 0;
+        };
+        return result;
     }
 }
