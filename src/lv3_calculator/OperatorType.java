0dc1c409d12f37ba92b0b91af24d88e237a69644
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-17 19:21:58 +0900

    Enum 구현

--- src/lv3_calculator/OperatorType.java
+++ src/lv3_calculator/OperatorType.java
@@ -1,5 +1,30 @@
 package lv3_calculator;
 
+// 람다를 실행할 익명 클래스 생성
+@FunctionalInterface
+interface Calculate {
+    int calculate(int a, int b);
+}
+
 public enum OperatorType {
-    /* 구현 */
+    // 상수 메서드
+    PLUS("+", (x, y) -> x + y),
+    MINUS("-", (x, y) -> x - y),
+    TIMES("*", (x, y) -> x * y),
+    DIVIDE("/", (x, y) -> x / y);
+
+    // 속성?
+    private Calculate cal; // 람다식을 생성할 필드
+    private String mark;
+
+    // 생성자
+    OperatorType(String mark, Calculate cal) {
+        this.mark = mark;
+        this.cal = cal;
+    }
+
+    //
+    public double execute(int x, int y) {
+        return cal.calculate(x, y);
+    }
 }
