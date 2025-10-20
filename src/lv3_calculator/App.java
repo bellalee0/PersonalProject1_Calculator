2885e83e5a6420dbb9a18d467013d2db70f0e656
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-20 10:44:37 +0900

    enum 활용하기

---
+++ src/lv3_calculator/App.java
@@ -0,0 +1,47 @@
+package lv3_calculator;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        ArithmeticCalculator calculator = new ArithmeticCalculator();
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = scanner.nextInt();
+            System.out.print("사칙연산 기호를 입력하세요(+, -, *, /): ");
+            char mark = scanner.next().charAt(0);
+            if (mark == '/' || mark == '%') {
+                System.out.println("※ 주의: 나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
+            }
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = scanner.nextInt();
+
+            int result = calculator.Calculate(num1, mark, num2);
+            String printing_result = num1 + " " + mark + " " + num2 + " = " + result;
+            System.out.println("계산 결과: " + printing_result);
+
+            calculator.setResult(printing_result);
+
+            if (calculator.getResult().size() > 3) {
+                calculator.removeResult();
+                System.out.println("현재까지 진행된 계산: " + calculator.getResult());
+            } else {
+                System.out.println("현재까지 진행된 계산: " + calculator.getResult());
+            }
+
+            scanner.nextLine();
+            System.out.println("더 계산하시겠습니까?(exit 입력 시 종료): ");
+            String exit = scanner.nextLine();
+
+            if (!exit.equals("exit")) {
+                System.out.println("새로운 계산을 시작합니다.");
+            } else {
+                break;
+            }
+        }
+    }
+}
--- src/lv3_calculator/ArithmeticCalculator.java
+++ src/lv3_calculator/ArithmeticCalculator.java
@@ -1,5 +1,36 @@
 package lv3_calculator;
 
-public class ArithmeticCalculator /* Hint */ {
-    /* 수정 */
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArithmeticCalculator {
+    private List<String> results = new ArrayList<String>();
+    OperatorType plus = OperatorType.PLUS;
+    OperatorType minus = OperatorType.MINUS;
+    OperatorType times = OperatorType.TIMES;
+    OperatorType divide = OperatorType.DIVIDE;
+
+    public int Calculate(int num1, char mark, int num2) {
+        int result = 0;
+        switch (mark) {
+            case '+' -> result = plus.execute(num1, num2);
+            case '-' -> result = minus.execute(num1, num2);
+            case '*', 'x', 'X' -> result = times.execute(num1, num2);
+            case '/', '%' -> result = divide.execute(num1, num2);
+            default -> System.out.println("Invalid mark");
+        }
+        return result;
+    }
+
+    List<String> getResult() {
+        return results;
+    }
+
+    void setResult(String result) {
+        results.add(result);
+    }
+
+    void removeResult() {
+        results.remove(0);
+    }
 }
