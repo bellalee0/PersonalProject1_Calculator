ea3290c5ebb9cd21a83a137ddf69b166af44697d
Seoyeon Lee <138790185+bellalee0@users.noreply.github.com>
2025-10-20 09:57:31 +0900

    Lv.2 계산기 Calculator 클래스 코드 리팩토링

--- src/lv2_calculator/Calculator.java
+++ src/lv2_calculator/Calculator.java
@@ -6,25 +6,13 @@ public class Calculator {
     private ArrayList<String> results = new ArrayList<String>();
 
     public int Calculate(int num1, char mark, int num2){
-        int result = 0;
-        switch(mark){
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*', 'x', 'X' -> result = num1 * num2;
-            case '/', '%' ->  result = num1 / num2;
-//            num2가 숫자인 경우, num2 < num1인 경우 처리 과정 추가 예정
-//            {
-//                if (num2 == 0) {
-//                    System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-//                } else if (num2 > num1) {
-//                    double d_num1 = num1;
-//                    double d_num2 = num2;
-//                    result = num1 / num2;
-//                } else {
-//                    result = num1 / num2;
-//                }
-//            }
-            default -> result = 0;
+        int result;
+        result = switch(mark){
+            case '+' -> num1 + num2;
+            case '-' -> num1 - num2;
+            case '*', 'x', 'X' -> num1 * num2;
+            case '/', '%' ->  num1 / num2;
+            default -> 0;
         };
         return result;
     }
